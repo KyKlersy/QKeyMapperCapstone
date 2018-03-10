ef70336e6d8b16d5c62831eaccfd6cbbc4bd9596
Omercan Devran <omercandevran@gmail.com>
2018-03-09 20:11:25 -0600

    Json object--will continue later..

--- GuiShellTest/LayoutEditorPage.xaml.cs
+++ GuiShellTest/LayoutEditorPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,62 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-//omercan was here
+
+public class JsonInfo {
+    private string keyname;
+    private string text;
+    private string width;
+    private string row;
+    private string column;
+    private string on_tap;
+
+    public JsonInfo(string keyname, string text, string width, string row, string column, string on_tap)
+    {
+        this.keyname = keyname;
+        this.text = text;
+        this.width = width;
+        this.row = row;
+        this.column = column;
+        this.on_tap = on_tap;
+    }
+
+    public string Width { get => width; set => width = value; }
+    public string Row { get => row; set => row = value; }
+    public string Column { get => column; set => column = value; }
+    public string Text { get => text; set => text = value; }
+    public string On_tap { get => on_tap; set => on_tap = value; }
+
+    public override string ToString() => String.Format(        // Same as Json format
+"{\"" + keyname + "\":{" + System.Environment.NewLine +
+
+"\"" + "graphics" + "\":{" + System.Environment.NewLine +
+
+"\"" + "width" + "\":" + width + "," + Environment.NewLine +
+"\"" + "row" + "\":" + row + "," + Environment.NewLine +
+"\"" + "column" + "\":" + column + "," + Environment.NewLine +
+"\"" + "text" + "\":" + "\"" + text + "\"" + "}," + Environment.NewLine +
+
+"\"" + "binding" + "\":" + Environment.NewLine +
+"\"" + "on_tap" + "\":" + "[" + on_tap + "] }," + Environment.NewLine +
+
+"\"" + "matrix" + "\":" + Environment.NewLine +
+"\"" + "row" + "\":" + row + "," + Environment.NewLine +
+"\"" + "column" + "\":" + column + "} };" + Environment.NewLine);
+}
+
+
+
 namespace QKeyMapper
 {
+
     /// <summary>
     /// Interaction logic for Page4.xaml
     /// </summary>
     public partial class LayoutEditorPage : Page
     {
+        List<JsonInfo> info = new List<JsonInfo>(1);   //Create List for JsonInfo class
+        
+
         public LayoutEditorPage()
         {
             InitializeComponent();
@@ -76,9 +125,34 @@ namespace QKeyMapper
 
         private void keyRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+       
             Rectangle rec = (Rectangle)sender;
             DataObject dataObj = new DataObject(rec);
             DragDrop.DoDragDrop(rec, dataObj, DragDropEffects.Move);
+           
+            
+
         }
+
+        private void createJson_Click(object sender, RoutedEventArgs e)
+        {
+            JsonInfo x = new JsonInfo("sad1", "2we", "3", "4", "w5", "e6");
+            info.Add(x);
+            // File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\" + layoutNameTextbox.Text +".json", info.ToString());
+
+            //System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\" + layoutNameTextbox.Text + ".json", info.ToString());
+            System.Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            
+           // System.Console.WriteLine(x);
+            info.ForEach(Console.WriteLine);
+
+
+            //TO BE CONTINUE
+
+            MessageBox.Show("Object is written to json file");
+
+        }
+
+     
     }
 }
