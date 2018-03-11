763428062efc6b01e40c11d5421cf54ee4c9ce91
Omercan Devran <omercandevran@gmail.com>
2018-03-11 15:36:23 -0600

    Serialize fixed

--- GuiShellTest/LayoutEditorPage.xaml.cs
+++ GuiShellTest/LayoutEditorPage.xaml.cs
@@ -15,49 +15,88 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Web.Script.Serialization;
 
-public class JsonObject
-{
-    public Key key { get; set; }
-}
-
-public class Key
-{
-    public Graphics graphics { get; set; }
-    public Binding binding { get; set; }
-    public Matrix matrix { get; set; }
-}
-
-public class Graphics
-{
-    public string width { get; set; }
-    public int row { get; set; }
-    public int column { get; set; }
-    public string text { get; set; }
-}
-
-public class Binding
-{
-    public object[] on_tap { get; set; }
-}
-
-public class Matrix
-{
-    public int Row { get; set; }
-    public int column { get; set; }
-}
 
 
 namespace QKeyMapper
 {
+
+    public class Rootobject
+    {
+        public string keyname { get; set; }
+        public Rootobject( string key ,string width, int row, int column, string text, object[] on_tap, int Matrixrow, int Matrixcolumn)
+        {
+            this.keyname = key;
+            new Graphics(width, row, column, text);
+            new Binding(on_tap);
+            new Matrix(Matrixrow, Matrixcolumn);
+
+        }
+
+        public Key key { get; set; }
+    }
+
+    public class Key
+    {
+        public Graphics graphics { get; set; }
+        public Binding binding { get; set; }
+        public Matrix matrix { get; set; }
+    }
+
+    public class Graphics
+    {
+        public Graphics(string width, int row, int column, string text)
+        {
+            this.width = width;
+            this.row = row;
+            this.column = column;
+            this.text = text;
+        }
+
+        public string width { get; set; }
+        public int row { get; set; }
+        public int column { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Binding
+    {
+        public Binding(object[] on_tap)
+        {
+            this.on_tap = on_tap;
+        }
+
+        public object[] on_tap { get; set; }
+    }
+
+    public class Matrix
+    {
+        public Matrix(int row, int column)
+        {
+            this.row = row;
+            this.column = column;
+        }
+
+        public int row { get; set; }
+        public int column { get; set; }
+    }
+
+
+
+
+
+
+
 
     /// <summary>
     /// Interaction logic for Page4.xaml
     /// </summary>
     public partial class LayoutEditorPage : Page
     {
-        List<Matrix> info = new List<Matrix>(1);   //row and column 
 
+
+        List<Rootobject> JsonInfo = new List<Rootobject>(1);  //List Contains Json Info
 
         public LayoutEditorPage()
         {
@@ -102,6 +141,8 @@ namespace QKeyMapper
 
 
 
+
+
                 }
 
             }
@@ -118,29 +159,33 @@ namespace QKeyMapper
             targetBorder.Child = copyRect;
 
         }
-      
+
         private void keyRect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Rectangle rec = (Rectangle)sender;
             DataObject dataObj = new DataObject(rec);
             DragDrop.DoDragDrop(rec, dataObj, DragDropEffects.Move);
-            Console.WriteLine(System.Windows.Input.Mouse.GetPosition(rec));
-            
+            // Console.WriteLine(Grid.GetColumn(rec));
+            //Console.WriteLine(Grid.GetRow(rec));
+      
+
+
         }
 
-       
+
 
 
         private void createJson_Click(object sender, RoutedEventArgs e)
         {
-            string KeyboardLayoutName = layoutNameTextbox.Text;
-            string KeyboardJSon = JsonConvert.SerializeObject(info, formatting: Formatting.Indented);
-            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\" + KeyboardLayoutName + ".json", KeyboardJSon);
-            Console.WriteLine("Go this address to open Json File:"+ AppDomain.CurrentDomain.BaseDirectory);
+
+            JsonInfo.Add(new Rootobject(key: "key1", width: "1u", row: 0, column: 0, text: "", on_tap: null , Matrixrow: 1 , Matrixcolumn: 1 ));
+
+            JavaScriptSerializer ser = new JavaScriptSerializer(); // Serializer 
+            string output = ser.Serialize(JsonInfo);                 //Serialize the List and add to output string
+            string KeyboardLayoutName = layoutNameTextbox.Text;     //Layout Name
+            System.IO.File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"\" + KeyboardLayoutName + ".json", output); //Save file
+            Console.WriteLine("Go this address to open Json File:" + AppDomain.CurrentDomain.BaseDirectory);     //File path
             MessageBox.Show("Keyboard Layout created in a Json file");
-            //string json = JsonConvert.SerializeObject(model);
-          //  File.WriteAllText("C:\json.txt", json);
-            //   info.ForEach(Console.WriteLine);
 
 
 
@@ -152,3 +197,20 @@ namespace QKeyMapper
 
 
 }
+
+/*
+public class KeyboardLayout
+{
+
+    public string KeyName { get; set; }
+    public int Row { get; set; }
+    public int Coloumn { get; set; }
+
+
+
+    public override string ToString()
+    {
+        return "keyname:" + this.KeyName + " ROW: " + this.Row + " Coloumn:" + this.Coloumn;
+    }
+}
+*/
