1b86358f21289e7cd9fb4252c5d10dcb496d8a1a
Kyle <KyKlersy@gmail.com>
2018-03-30 22:40:15 -0500

    Quick changes to the visual layout editor page reflects the discussion we had today about how to make it look. Added control debugs so you no longer need to click to load a panel, see mainwindow.cs file there is a debug integer set it to the number that it represents and it should load that panel bypassing the first panel.

--- GuiShellTest/MainWindow.xaml.cs
+++ GuiShellTest/MainWindow.xaml.cs
@@ -22,8 +22,36 @@ namespace QKeyMapper
     {
         public MainWindow()
         {
+            //0 set to 0 to load default panel
+            //1 set to 1 to load layout editor panel
+            //2 set to 2 to load binding editor panel
+            //3 set to 3 to load macro editor panel
+            //4 set to 4 to load flashing page panel
+            int panelDebug = 1;
+
             InitializeComponent();
-            mainFrame.Content = new KeyBoardInfoPage();
+
+            switch(panelDebug)
+            {
+                case 0:
+                    mainFrame.Content = new KeyBoardInfoPage();
+                    break;
+                case 1:
+                    mainFrame.Content = new LayoutEditorPage();
+                    break;
+                case 2:
+                    mainFrame.Content = new BindingEditorPage();
+                    break;
+                case 3:
+                    mainFrame.Content = new MacroEditorPage();
+                    break;
+                case 4:
+                    mainFrame.Content = new FlashingPage();
+                    break;
+            }
+
+
+            //mainFrame.Content = new KeyBoardInfoPage();
         }
     }
 }
