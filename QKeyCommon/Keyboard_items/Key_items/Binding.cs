40780f5d1c561382a27464c60e35c8ff770b1fb2
xtodpep_stew <chris.tstew@gmail.com>
2018-04-27 16:57:50 -0500

    removed on_up from Binding.cs

--- QKeyCommon/Keyboard_items/Key_items/Binding.cs
+++ QKeyCommon/Keyboard_items/Key_items/Binding.cs
@@ -10,12 +10,10 @@ namespace QKeyCommon.Keyboard_items.Key_items
     {
         public List<string> on_tap;
         public List<string> on_hold;
-        public List<string> on_up;
         public Binding()
         {
             this.on_hold = new List<string>();
             this.on_tap = new List<string>();
-            this.on_up = new List<string>();
         }
     }
 }
